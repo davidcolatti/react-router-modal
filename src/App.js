5c231af022bd42043da7dd034cd88ce61b814034
David Colatti <davidcolatti@MacBook-Pro.local>
2020-06-22 16:32:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { ModalRoute, ModalContainer } from "react-router-modal";
+
+// This is the css for the modal
+import "react-router-modal/css/react-router-modal.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ModalContainer />
+      <h1>This Is My Main App Screen</h1>
+      <Link to="/modal-test">Click Me To Show The Modal</Link>
+
+      <ModalRoute path="/modal-test" parentPath="/">
+        {/* this would be where my modal component can be inside */}
+        Hello
+      </ModalRoute>
     </div>
   );
 }
